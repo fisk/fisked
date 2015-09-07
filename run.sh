a6f10954ec7992cebdfe38ffb7f85776143db57b
eosterlund <erik.osterlund@lnu.se>
2015-09-07 12:36:32 +0200

    stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath bin:lib/jcurses.jar org.fisked.Main
+java -Djava.library.tmpdir="/tmp/org.fisked" -jar fisked-executable/target/fisked-executable-0.0.1-SNAPSHOT-osx-assembly.jar
