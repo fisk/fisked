0e86769dcab3447b4d1fd00e4c586a62ee3bee75
Erik Österlund <fisksvett@gmail.com>
2020-03-28 23:46:37 +0100

    Remove transitive dependencies

--- src/main/java/org.fisk.fisked/module-info.java
+++ src/main/java/org.fisk.fisked/module-info.java
@@ -1,14 +1,7 @@
 module org.fisk.fisked {
-    requires gson;
-    requires guava;
     requires lanterna;
     requires lucene.core;
     requires org.eclipse.lsp4j;
-    requires org.eclipse.lsp4j.generator;
-    requires org.eclipse.lsp4j.jsonrpc;
-    requires org.eclipse.xtend.lib;
-    requires org.eclipse.xtend.lib.macro;
-    requires org.eclipse.xtext.xbase.lib;
     requires org.slf4j;
     requires org.slf4j.simple;
 }
