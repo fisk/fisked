892aa01a0688af474278cbdb4c1d89a8d14388d9
Erik Österlund <fisksvett@gmail.com>
2020-07-15 01:01:28 +0200

    better file indexing

--- src/main/java/org.fisk.fisked/org/fisk/fisked/fileindex/FileIndex.java
+++ src/main/java/org.fisk.fisked/org/fisk/fisked/fileindex/FileIndex.java
@@ -3,14 +3,17 @@ package org.fisk.fisked.fileindex;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.fisk.fisked.ui.Window;
 import org.fisk.fisked.ui.ListView.ListItem;
+import org.fisk.fisked.ui.Window;
+import org.fisk.fisked.utils.LogFactory;
+import org.slf4j.Logger;
 
 public class FileIndex {
+    private static final Logger _log = LogFactory.createLog();
+    
     private static class FileIndexItem extends ListItem {
         private Path _root;
         private Path _path;
@@ -41,6 +44,17 @@ public class FileIndex {
                        Integer.MAX_VALUE,
                        (filePath, fileAttr) -> fileAttr.isRegularFile())
             .forEach((path) -> {
+                _log.info("Menu path: " + path);
+                var relative = root.relativize(path);
+                _log.info("Relative menu path: " + relative);
+                for (int i = 0; i < relative.getNameCount(); ++i) {
+                    var name = relative.getName(i);
+                    _log.info("Menu path component: " + name);
+                    if (name.toString().substring(0, 1).equals(".")) {
+                        _log.info("Invisible component");
+                        return;
+                    }
+                }
                 list.add(new FileIndexItem(root, path));
             });
         } catch (IOException e) {
